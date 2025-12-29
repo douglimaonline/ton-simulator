a570080948b859458296af05710d8b306e9898f8
douglas <douglimaonline@gmail.com>
2025-12-29 17:18:58 -0300

    Update Plan Service for production enviroment

--- src/app/services/plan.service.ts
+++ src/app/services/plan.service.ts
@@ -17,7 +17,7 @@ export class PlanService {
   async editPlan(
     plan: Plan
   ): Promise<{ success: boolean; plan?: string; message: string }> {
-    const planRef = doc(db, 'Plans test', plan.id.toString()); // change for 'Plans' in production
+    const planRef = doc(db, 'Plans', plan.id.toString()); // change for 'Plans' in production
 
     try {
       await setDoc(planRef, {
@@ -42,7 +42,7 @@ export class PlanService {
 
   private async fetchPlans(): Promise<Plan[]> {
     const plans: Plan[] = [];
-    const q = query(collection(db, 'Plans test')); // change for 'Plans' in production
+    const q = query(collection(db, 'Plans')); // change for 'Plans' in production
     const querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc) => {
